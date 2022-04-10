f3c61462054386cc2bf9c83f1647ec079952b6bd
Mateus Marwell <marwell.dc@gmail.com>
2022-04-10 00:43:39 -0300

    removal of unnecessary imports

--- Migrations/20220408173722_InitialCreate.Designer.cs
+++ Migrations/20220408173722_InitialCreate.Designer.cs
@@ -3,7 +3,6 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
 using Microsoft.EntityFrameworkCore.Migrations;
-using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using ShopWebApi.Data;
 
 namespace ShopWebApi.Migrations
